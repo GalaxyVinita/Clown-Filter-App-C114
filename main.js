a52f6b817c187a5136f4cc97bf1744928ae933b7
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2021-10-27 20:39:51 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/MHLnz5Sb/Clown-Nose-P114.png');
 }
 
 function setup(){
@@ -22,14 +26,17 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 
 function draw(){
     image(video,0,0,300,300);
+    image(clown_nose, noseX, noseY,30,30);
 }
 
 function take_snapshot(){
